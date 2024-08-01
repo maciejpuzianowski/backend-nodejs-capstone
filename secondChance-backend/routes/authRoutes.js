327d1f247846dd5ed960665254d8455e81091b39
maciejpuzianowski <maciek.puzianowski@gmail.com>
2024-08-01 07:08:15 -0400

    register

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,40 +1,51 @@
 const express = require('express');
-const router = express.Router();
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const connectToDatabase = require('../models/db');
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken")
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+dotenv.config();
 
-const JWT_SECRET = "SECRET"
+const logger = pino();  // Create a Pino logger instance
+
+//Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-        const db = await connectToDatabase();       
-        const collection = db.collection("users");
-        const existingEmail = await collection.findOne({ email: req.body.email });
+      //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+      const db = await connectToDatabase();
+      const collection = db.collection("users");
+      const existingEmail = await collection.findOne({ email: req.body.email });
+
         if (existingEmail) {
-            console.log('Email id already exists');
+            logger.error('Email id already exists');
             return res.status(400).json({ error: 'Email id already exists' });
         }
+
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
+        const email=req.body.email;
         const newUser = await collection.insertOne({
-                email: req.body.email,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                password: hash,
-                createdAt: new Date(),
-            });
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
         const payload = {
-                    user: {
-                        id: newUser.insertedId,
-                    },
-                };
+            user: {
+                id: newUser.insertedId,
+            },
+        };
 
         const authtoken = jwt.sign(payload, JWT_SECRET);
-        console.log('User registered successfully');
-        res.json({ authtoken,email: req.body.email });
+        logger.info('User registered successfully');
+        res.json({ authtoken,email });
     } catch (e) {
-        console.log(e)
+        logger.error(e);
         return res.status(500).send('Internal server error');
     }
 });
